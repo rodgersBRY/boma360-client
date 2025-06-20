655400084895c06739cbba1b0501d6cf820141f4
Mawira <brian.mawi@yahoo.com>
2025-06-20 11:54:45 +0300

    fix

--- lib/presentation/pages/main/main_controller.dart
+++ lib/presentation/pages/main/main_controller.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 
 class MainController extends GetxController {
-  var selectedIndex = 1.obs;
+  var selectedIndex = 0.obs;
 
   void changeIndex(int index) {
     selectedIndex.value = index;
