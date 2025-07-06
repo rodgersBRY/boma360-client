55cf6c756d5cec199b5253d5c4287f7238be8154
Mawira <brian.mawi@yahoo.com>
2025-07-07 00:21:34 +0300

    fix

--- lib/model/notification.g.dart
+++ lib/model/notification.g.dart
@@ -26,20 +26,6 @@ AppNotification _$AppNotificationFromJson(Map<String, dynamic> json) =>
               : DateTime.parse(json['updatedAt'] as String)
       ..deleted = json['deleted'] as bool?;
 
-Map<String, dynamic> _$AppNotificationToJson(AppNotification instance) =>
-    <String, dynamic>{
-      'id': instance.id,
-      'createdAt': instance.createdAt?.toIso8601String(),
-      'updatedAt': instance.updatedAt?.toIso8601String(),
-      'deleted': instance.deleted,
-      'iconData': instance.iconData,
-      'tag': _$NotificationTagEnumMap[instance.tag]!,
-      'title': instance.title,
-      'body': instance.body,
-      'date': instance.date.toIso8601String(),
-      'priority': _$NotificationPriorityEnumMap[instance.priority]!,
-    };
-
 const _$NotificationTagEnumMap = {
   NotificationTag.urgent: 'urgent',
   NotificationTag.health: 'health',
